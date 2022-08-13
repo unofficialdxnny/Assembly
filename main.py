b41e8dad3f51af2fd51f6ebb1369c46acad0523f
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-13 23:55:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from ast import excepthandler
+from ftplib import all_errors
+import os, getpass, requests
+from pystyle import *
+import urllib.request
+
+banner = '''
+
+ █████╗ ███████╗███████╗███████╗███╗   ███╗██████╗ ██╗  ██╗   ██╗
+██╔══██╗██╔════╝██╔════╝██╔════╝████╗ ████║██╔══██╗██║  ╚██╗ ██╔╝
+███████║███████╗███████╗█████╗  ██╔████╔██║██████╔╝██║   ╚████╔╝ 
+██╔══██║╚════██║╚════██║██╔══╝  ██║╚██╔╝██║██╔══██╗██║    ╚██╔╝  
+██║  ██║███████║███████║███████╗██║ ╚═╝ ██║██████╔╝███████╗██║   
+╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝   
+                 Windows Setup for unofficialdxnny                                                
+                                                                 
+'''
+os.system('cls & title Windows Setup ~ unofficialdxnny')
+print(Colorate.Horizontal(Colors.red_to_black, Center.XCenter(banner)))
+
