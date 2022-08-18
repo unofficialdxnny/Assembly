3c8d6bcd975b0d71d218ef1022ee6e5f1057557a
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-18 12:37:55 +0100

    Added custom cursors (need to make more efficient)

--- main.py
+++ main.py
@@ -20,28 +20,13 @@ banner = '''
                                                                  
 '''
 
-granted = '''
-
- ▄▄▄       ▄████▄   ▄████▄  ▓█████   ██████   ██████      ▄████  ██▀███   ▄▄▄       ███▄    █ ▄▄▄█████▓▓█████ ▓█████▄ 
-▒████▄    ▒██▀ ▀█  ▒██▀ ▀█  ▓█   ▀ ▒██    ▒ ▒██    ▒     ██▒ ▀█▒▓██ ▒ ██▒▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒▓█   ▀ ▒██▀ ██▌
-▒██  ▀█▄  ▒▓█    ▄ ▒▓█    ▄ ▒███   ░ ▓██▄   ░ ▓██▄      ▒██░▄▄▄░▓██ ░▄█ ▒▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░▒███   ░██   █▌
-░██▄▄▄▄██ ▒▓▓▄ ▄██▒▒▓▓▄ ▄██▒▒▓█  ▄   ▒   ██▒  ▒   ██▒   ░▓█  ██▓▒██▀▀█▄  ░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ ▒▓█  ▄ ░▓█▄   ▌
- ▓█   ▓██▒▒ ▓███▀ ░▒ ▓███▀ ░░▒████▒▒██████▒▒▒██████▒▒   ░▒▓███▀▒░██▓ ▒██▒ ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ ░▒████▒░▒████▓ 
- ▒▒   ▓▒█░░ ░▒ ▒  ░░ ░▒ ▒  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░    ░▒   ▒ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   ░░ ▒░ ░ ▒▒▓  ▒ 
-  ▒   ▒▒ ░  ░  ▒     ░  ▒    ░ ░  ░░ ░▒  ░ ░░ ░▒  ░ ░     ░   ░   ░▒ ░ ▒░  ▒   ▒▒ ░░ ░░   ░ ▒░    ░     ░ ░  ░ ░ ▒  ▒ 
-  ░   ▒   ░        ░           ░   ░  ░  ░  ░  ░  ░     ░ ░   ░   ░░   ░   ░   ▒      ░   ░ ░   ░         ░    ░ ░  ░ 
-      ░  ░░ ░      ░ ░         ░  ░      ░        ░           ░    ░           ░  ░         ░             ░  ░   ░    
-          ░        ░                                                                                           ░      
-
-
-'''
 
 os.system('cls')
 print(Colorate.Horizontal(Colors.red_to_black, Center.XCenter(banner)))
 username = getpass.getuser()
-name = Write.Input("What is your username", Colors.red_to_black, interval=0.0025)
+name = Write.Input("What is your username : ", Colors.red_to_black, interval=0.0025)
 if name == username:
-    Write.Print(f"{granted}", Colors.red_to_black, interval=0.001)
+    ## Write.Print(f"{granted}", Colors.red_to_black, interval=0.001)
     def is_admin():
 
         try:
@@ -64,26 +49,39 @@ if name == username:
                 print(Colors.green + f'Application {lines} installed')
         
         def cursors():
-            with open(f'cur-loc.txt', 'r+') as cursors:
-                line = cursors.readlines()
-                for lines in line:
-                    path = r"HKEY_CURRENT_USER\Control Panel\Cursors"
-                    cur_loc = f"./Cursors/{cursors}.ani"
+            ## while x < 7:
+            ##     with open(f'cur-loc.txt', 'r+') as cursors:
+            ##         line = cursors.readlines()
+            ##         for lines in line:
+            path = r"HKEY_CURRENT_USER\Control Panel\Cursors"
+            ## cur_loc = f"./Cursors/{lines}.ani"
             
-            with open('type.txt', 'r+') as cur_type:
-                line = cur_type.readlines()
-                for lines in line:
-                    type = line
+            type = ['Arrow', 'AppStarting', 'Crosshair', 'Hand', 'Help', 'No', 'NWPen', 'Wait']
+            x = 0
 
 
-            os.system(f"""REG ADD "{path}" /v {type} /t REG_EXPAND_SZ /d "{cur_loc}" /f""")
-
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Normal Select.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Working in Background.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Precission Select.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Link Select.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Help Select.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Unavailable.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Handwritting.ani" /f""")
+            x = +1
+            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Busy.ani" /f""")
             ctypes.windll.user32.SystemParametersInfoA(0x57)
-        
+                    
+
         cursors()
         ##choco_install()
     
-        sleep(5)
+        
     else:
         # Re-run the program with admin rights
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
