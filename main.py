154d440569ef1a0e689916a506b6aaa5b0cff642
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-20 23:40:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from ast import excepthandler
-from ftplib import all_errors
-import os, getpass, requests
-from traceback import format_exception
+import os
+import getpass
+import requests
+import speedtest
+import json
 from pystyle import *
-import urllib.request
-import ctypes
-import sys
-from urllib.parse import urlparse
+from time import sleep
+
 
 banner = '''
 
@@ -20,77 +19,11 @@ banner = '''
                                                                  
 '''
 
+os.system('cls & title Assembly ~ unofficialdxnny & color a1')
 
-os.system('cls')
-print(Colorate.Horizontal(Colors.red_to_black, Center.XCenter(banner)))
-username = getpass.getuser()
-name = Write.Input("What is your username : ", Colors.red_to_black, interval=0.0025)
-if name == username:
-    ## Write.Print(f"{granted}", Colors.red_to_black, interval=0.001)
-    def is_admin():
+print(Colorate.Horizontal(Colors.red_to_purple, "Getting things ready", 1))
 
-        try:
-            return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
-            return False
-    
-    if is_admin():
-        os.system('cls & title Windows Setup ~ unofficialdxnny')
-        print(Colorate.Horizontal(Colors.red_to_black, Center.XCenter(banner)))
-    
-        ## install chocolatey files
-        def choco_install():
-            print(Colors.green + 'Installing chocolatey applications')
-            ## os.system("powershell -command Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.Servic
-            with open(f'choco.txt', 'r+') as choco:
-                line = choco.readlines()
-                for lines in line:
-                    os.system(f'choco install {lines} -y --force')
-                print(Colors.green + f'Application {lines} installed')
-        
-        def cursors():
-            ## while x < 7:
-            ##     with open(f'cur-loc.txt', 'r+') as cursors:
-            ##         line = cursors.readlines()
-            ##         for lines in line:
-            path = r"HKEY_CURRENT_USER\Control Panel\Cursors"
-            ## cur_loc = f"./Cursors/{lines}.ani"
-            
-            type = ['Arrow', 'AppStarting', 'Crosshair', 'Hand', 'Help', 'No', 'NWPen', 'Wait', 'UpArrow', 'SizeAll', '']
-            x = 0
+## Change window title and prints banner
+print(Colorate.Horizontal(Colors.red_to_purple, Center.XCenter(banner)))
 
-
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Normal Select.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Working in Background.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Precission Select.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Link Select.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Help Select.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Unavailable.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Handwritting.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Busy.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Alternative Select.ani" /f""")
-            x = +1
-            os.system(f"""REG ADD "{path}" /v {type[x]} /t REG_EXPAND_SZ /d "./Cursors/Move.ani" /f""")
-            ctypes.windll.user32.SystemParametersInfoA(0x57)
-                    
-
-        cursors()
-        ##choco_install()
-    
-        
-    else:
-        # Re-run the program with admin rights
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-
-
-
-    
 
