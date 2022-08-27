7c678b7218912208fb646b70ebbd93ec5a1a804c
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-27 21:48:00 +0100

    Added Spotify Premium

--- main.py
+++ main.py
@@ -37,32 +37,32 @@ def choco_installer():
     choco_path = r'C:/ProgramData/chocolatey'
 
     choco_path_exists = os.path.isdir(choco_path)
-    ## print(Colorate.Horizontal(Colors.red_to_purple, f"{choco_path_exists}", 1))
+    ## print(Colorate.Horizontal(Colors.red_to_purple,f"{choco_path_exists}", 1))
 
     if choco_path_exists == 'False':
-        print(Colors.red, f"Chocolatey Not Found. Installing...'")
+        print(Colors.red,f"Chocolatey Not Found. Installing...'")
         choco_install = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
         os.startfile("powershell.exe")
         sleep(3)
         kb.write(choco_install)
         kb.press('enter')
     else:
-        print(Colors.red, f"Chocolatey Installation Found")
+        print(Colors.red,f"Chocolatey Installation Found")
         sleep(2)
-        print(Colors.green, f"Installing Selected Applications")
+        print(Colors.green,f"Installing Selected Applications")
         with open('choco.ps1', 'r+') as chocolatey_application_install:
             ## os.startfile("powershell.exe")
             sleep(2)
             line = chocolatey_application_install.readlines()
             for lines in line:
                 os.system(f"choco install {lines} -y --force")
-                print(Colors.red, f"Installation of {lines} completed!'")
+                print(Colors.red,f"Installation of {lines} completed!'")
 
                 
 
 def aesthetics():
     ## Change mouse to .ani
-    print(Colors.red, f"Changing mouse settings...'")
+    print(Colors.red,f"Changing mouse settings...'")
     path = r"HKEY_CURRENT_USER\Control Panel\Cursors"
     cur_loc = r"./Cursors/Normal Select.ani"
     os.system(f"""REG ADD "{path}" /v Default /t REG_EXPAND_SZ /d "{cur_loc}" /f""")
@@ -84,10 +84,16 @@ def aesthetics():
     os.system(f"""REG ADD "{path}" /v UpArrow /t REG_EXPAND_SZ /d "{cur_loc}" /f""")
     os.system(f"""REG ADD "{path}" /v Wait /t REG_EXPAND_SZ /d "{cur_loc}" /f""")
     ctypes.windll.user32.SystemParametersInfoA(0x57)
-    print(Colors.red, f"Completed...'")
+    print(Colors.red,f"Completed...'")
+
+def spotify():
+    print(Colors.green,f"Installing Spotify Premium")
+    os.system('SpotXBasic.bat')
+    print(Colors.green,f"Completed Successfully")
 
     
 
 
-aesthetics()
-## choco_installer()
+spotify() ## Installs Spotify Premium Patch
+aesthetics() ## Sets visuals up
+## choco_installer() ## Installs applications needed
